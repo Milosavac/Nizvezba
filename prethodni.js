81ac0fbfe1c5c600b3bc7d0dab78a318ff4fe8bb
Milosavac <miodragmilosavac@gmail.com>
2017-02-20 23:24:17 +0100

    Add files via upload

---
+++ prethodni.js
@@ -0,0 +1,18 @@
+var current=document.getElementById('current');
+var slicica=document.getElementsByClassName('thumb');
+
+var a=[5,6,19,'k','l'];	
+
+var poz=a.indexOf('k');
+
+
+
+var pozicija=slicica.indexOf(current);	// u ovom redu bi trebao da uzmem index
+
+
+
+
+
+//meni treba pozicija slike current u nizu slicica
+
+
